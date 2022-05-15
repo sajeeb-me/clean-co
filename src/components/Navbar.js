95c06055ad2c14190887f6228c6b499cc069164c
sdshuvo98 <hellosdshuvo@gamil.com>
2022-05-16 00:18:03 +0600

    theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { useEffect } from 'react'
-import { themeChange } from 'theme-change'
 
 const Navbar = ({ children }) => {
+    const [darkTheme, setDarkTheme] = useState(false);
 
-    useEffect(() => {
-        themeChange(false)
-    }, [])
 
     const navItems = <>
         <li><NavLink className="rounded-lg" to='/'>Home</NavLink></li>
@@ -24,7 +20,7 @@ const Navbar = ({ children }) => {
         </li>
         <label className="swap swap-rotate m-5 lg:m-0">
 
-            <input data-toggle-theme="dark,light" type="checkbox" />
+            <input onClick={() => setDarkTheme(!darkTheme)} type="checkbox" />
 
             <svg className="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
 
@@ -34,7 +30,7 @@ const Navbar = ({ children }) => {
     </>
 
     return (
-        <div className="drawer drawer-end">
+        <div className="drawer drawer-end" data-theme={darkTheme ? 'dark' : 'light'}>
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
                 {/* <!-- Navbar --> */}
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div class="hero min-h-screen bg-base-200">
+            <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div class="card-body">
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="email" class="input input-bordered" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Password</span>
+                        </label>
+                        <input type="text" placeholder="password" class="input input-bordered" />
+                        <label class="label">
+                            <Link to='/login' class="label-text-alt link link-hover">Forgot password?</Link>
+                        </label>
+                    </div>
+                    <div class="form-control mt-6">
+                        <button class="btn btn-primary">Login</button>
+                    </div>
+                    <div class="divider">OR</div>
+                    <button className='btn btn-outline'>Continue with Google</button>
+                </div>
+            </div>
         </div>
     );
 };
